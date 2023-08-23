cb6694d3eadda281c609fe2a22f98ea2018afebc
SmarakNayak <miltu.s.nayak@gmail.com>
2023-08-23 23:33:33 +1000

    changed favicon and added edition

--- app/src/pages/Inscription.js
+++ app/src/pages/Inscription.js
@@ -5,20 +5,33 @@ import { lightTheme } from '../styles/themes';
 
 const Inscription = () => {
   let { number } = useParams();
-  const [content, setContent] = useState(null);
+  const [binaryContent, setBinaryContent] = useState(null);
+  const [blobUrl, setBlobUrl] = useState(null);
+  const [textContent, setTextContent] = useState(null);
   const [metadata, setMetadata] = useState(null);
+  const [editions, setEditions] = useState(null);
+  const [editionNumber, setEditionNumber] = useState(null);
   const [contentType, setContentType] = useState(null);
   const [nextNumber, setNextNumber] = useState(null);
   const [previousNumber, setPreviousNumber] = useState(null);
   const [randomNumber, setRandomNumber] = useState(null);
 
   useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch("/api/inscription_number_metadata/"+number);
-      const json = await response.json();
-      console.log(json);
-      setMetadata(json);
-      switch (json?.content_type) {
+
+    const fetchContent = async () => {
+      setBlobUrl(null);
+      setTextContent(null);
+      setContentType("loading");
+      //1. Get content
+      const response = await fetch("/api/inscription_number/"+number);
+      //2. Assign local url
+      const blob = await response.blob();
+      const url = URL.createObjectURL(blob);
+      setBinaryContent(blob);
+      setBlobUrl(url);
+      //3. Work out type
+      let content_type = response.headers.get("content-type");
+      switch (content_type) {
         //Image types
         case "image/png":
           setContentType("image");
@@ -82,41 +95,69 @@ const Inscription = () => {
           break;
       }
     }
-    const fetchContent = async () => {
-      const content = await fetch("/api/inscription_number/"+number);
-      const text = await content.text();
-      setContent(text);
+    
+    const fetchMetadata = async () => {
+      const response = await fetch("/api/inscription_number_metadata/"+number);
+      const json = await response.json();
+      setMetadata(json);
     }
-    setContent("");
-    fetchData();
+
+    const fetchEditions = async () => {
+      setEditionNumber(null);
+      const response = await fetch("/api/inscription_number_editions/"+number);
+      const json = await response.json();
+      setEditions(json);
+      for (let index = 0; index < json.length; index++) {
+        const element = json[index];
+        if(element.number==number){
+          setEditionNumber(element.edition);
+        }    
+      }
+    }
+
     fetchContent();
+    fetchMetadata();
+    fetchEditions();
     setNextNumber(parseInt(number)+1);
     setPreviousNumber(parseInt(number)-1);
     setRandomNumber(Math.floor(Math.random() * 1000001));
   },[number])
 
+  useEffect(()=> {
+    const updateText = async () => {
+      //1. Update text state variable if text type
+      if(contentType=="text") {
+        const text = await binaryContent.text();
+        setTextContent(text);
+      }
+    }
+    updateText();    
+  },[contentType])
+
   return (
     <PageContainer>
+      <Heading>Inscription {metadata?.number}</Heading>
       {
         {
-          'image': <ImageContainer src={"/api/inscription_number/"+number} />,
-          'html': <HtmlContainer src={"/api/inscription_number/"+number}/>, //
-          'text': <TextContainer>{content}</TextContainer>,
-          'video': <video controls loop muted autoplay><source src={"/api/inscription_number/"+number} type={metadata?.content_type}/></video>,
-          'audio': <audio controls><source src={"/api/inscription_number/"+number} type={metadata?.content_type}/></audio>,
+          'image': <ImageContainer src={blobUrl} />,
+          'html': <HtmlContainer src={blobUrl}/>, //
+          'text': <TextContainer>{textContent}</TextContainer>,
+          'video': <video controls loop muted autoplay><source src={blobUrl} type={metadata?.content_type}/></video>,
+          'audio': <audio controls><source src={blobUrl} type={metadata?.content_type}/></audio>,
           'pdf': <TextContainer>pdf unsupported'</TextContainer>,
           'model': <TextContainer>gltf model type unsupported</TextContainer>,
-          'unsupported': <p>{metadata?.content_type} content type unsupported</p>
+          'unsupported': <TextContainer>{metadata?.content_type} content type unsupported</TextContainer>,
+          'loading': <TextContainer>loading...</TextContainer>
         }[contentType]
       }
-      <div>
-        <p>Number: {metadata?.number}</p>
+      <div>        
         <p>Id: {metadata?.id}</p>
         <p>Style: {metadata?.content_type}</p>
         <p>Size: {metadata?.content_length}</p>
         <p>Fee: {metadata?.genesis_fee}</p>
         <p>Inscription Blocktime: {metadata?.genesis_height} </p>
         <p>Inscription Clocktime: {metadata?.timestamp} </p>
+        <p>Edition: {editionNumber} </p>
         <LinksContainer>
           <Link to={'/inscription/' + previousNumber}> previous </Link>
           <Link to={'/inscription/' + randomNumber}> discover </Link>
@@ -153,13 +194,26 @@ const LinksContainer = styled.div`
 `;
 
 const ImageContainer = styled.img`
-  width: 16rem;
-  height: 16rem;
+  min-height:16rem;
+  min-height:16rem;
+  max-width:32rem;
+  max-height:32rem;
+  width: auto;
+  height: auto;
   image-rendering: pixelated;
 `;
 
+const Heading = styled.h2`
+  font-family: monospace;
+`
+
 const TextContainer = styled.p`
-  margin-bottom: 100px;
+  max-width: 800px;
+  font-size: 1em;
+  display: block;
+  font-family: monospace;
+  white-space-collapse: preserve;
+  margin: 10em 10em;
 `;
 
 const HtmlContainer = styled.iframe`
