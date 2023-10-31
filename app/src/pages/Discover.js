a8edff4c6a812b89cd79960dcdeef20935dbf2c8
SmarakNayak <miltu.s.nayak@gmail.com>
2023-10-31 22:45:35 +1100

    0.5 threshold flickers, trying 0.9

--- app/src/pages/Discover.js
+++ app/src/pages/Discover.js
@@ -45,7 +45,7 @@ const Discover = () => {
           fetchInscriptions();
         }
       },
-      { threshold: 0.5 }
+      { threshold: 0.9 }
     );
 
     
@@ -69,7 +69,7 @@ const Discover = () => {
           fetchInscriptions()
         }
       },
-      { threshold: 0.5 }
+      { threshold: 0.9 }
     );
 
     if (observerTarget.current) {
