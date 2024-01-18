6b403d0edb68cc6d8be2f68f5bec0b43e55e6e89
SmarakNayak <miltu.s.nayak@gmail.com>
2024-01-18 16:58:57 +1100

    CI/CD changes

--- systemd/memory_watcher.sh
+++ systemd/memory_watcher.sh
@@ -3,11 +3,11 @@ while true
 do
   m=`free -m | head -n 2 |tail -n 1 | tr -s " " | cut -d " " -f 3`
   echo "Memory used: $m"
-  if [ $m -ge 15000 ]; then
+  if [ $m -ge 31000 ]; then
     echo "Way too much memory, Hard killing"
     systemctl kill -s SIGKILL ordserver.service&
     sleep 50
-  elif [ $m -ge 13500 ]; then
+  elif [ $m -ge 29000 ]; then
     echo "Memory getting high, Soft killing"
     systemctl restart ordserver.service&
   fi
