ed4920a447c0f73b34cbd1cfcb4b1319b6ba5c65
Smarak Nayak <miltu.s.nayak@gmail.com>
2025-03-06 21:19:42 +1100

    add timing

--- app/server/server.js
+++ app/server/server.js
@@ -12,40 +12,6 @@ const server = Bun.serve({
   port: 1082,
   routes: {
     '/': new Response('Hello World!'),
-    '/content/:id': async req => {
-      const url = new URL(req.url)
-      let searchParams = url.searchParams;
-
-      // This should only trigger for recursive html, nginx will not route other requests here
-      // We reroute just in case
-      if (!searchParams.has('vermilion_ssr')) {
-        let newHeaders = new Headers();
-        newHeaders.set('accept-encoding', req.headers.get('accept-encoding') || 'identity');
-        return fetch(apiBaseUrl + "/content/" + req.params.id + url.search, {
-          headers: newHeaders,
-          decompress: false, // Do not decompress response
-        });
-      }
-
-      // Otherwise, remove the flag and continue
-      searchParams.delete('vermilion_ssr');
-      if (!playwrightBrowser) {
-        playwrightBrowser = await chromium.launch({ headless: true, args: ['--no-sandbox'] });
-      }
-      let newUrl = apiBaseUrl + "/content/" + req.params.id + url.search;
-      let htmlResponse = await fetch(newUrl, {
-        decompress: false,
-      });
-      let htmlContent = await htmlResponse.text();
-      const prefetchLinks = await extractPrefetchLinks(newUrl);
-      // Join prefetch links with newlines and inject into head section
-      let modifiedHtml = appendPrefetchLinks(htmlContent, prefetchLinks);
-
-      console.log(modifiedHtml);
-      return new Response(modifiedHtml, {
-        headers: { 'Content-Type': 'text/html' },
-      });
-    },
     '/renderedContentPlaywright/:id': async req => {
       let ss = await renderContentPlaywright(apiBaseUrl + "/content/" + req.params.id);
       if (!ss) {
@@ -67,87 +33,13 @@ const server = Bun.serve({
   }
 });
 
-// This renders the content using Playwright, capturing all /content requests made
-// we can then inject the prefetch links into the head section
-// saving the client side waterfall as all requests are made in parallel
-async function extractPrefetchLinks(url) {
-  const page = await playwrightBrowser.newPage();
-  const contentLinks = new Set();
-
-  await page.route('**/*', async route => {
-    let path = new URL(route.request().url()).pathname;
-    if (path.startsWith('/content')) {
-      contentLinks.add(path);
-    }
-    await route.continue();
-  });
-  await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 10000 });
-  await page.waitForLoadState('networkidle', { timeout: 10000 });
-
-  const prefetchLinks = Array.from(contentLinks).map(
-    path => `<link rel="prefetch" href="${path}" />`
-  );
-  
-  return prefetchLinks;
-}
-
-function appendPrefetchLinks(htmlContent, prefetchLinks) {
-  const prefetchTags = prefetchLinks.join('\n    ');
-  console.log(htmlContent);
-  let modifiedHtml = htmlContent;
-  if (htmlContent.includes('<head>')) {
-    modifiedHtml = htmlContent.replace(
-      '<head>',
-      `<head>\n    ${prefetchTags}`
-    );
-  } else {
-    modifiedHtml = `<!DOCTYPE html><html><head>\n    ${prefetchTags}\n</head><body>${htmlContent}</body></html>`;
-  }
-  console.log(modifiedHtml);
-  return modifiedHtml;
-}
-
 async function renderContentPlaywright(url) {
-  // let startTime = performance.now();
+  let startTime = performance.now();
   if (!playwrightBrowser) {
     playwrightBrowser = await chromium.launch({ headless: true, args: ['--no-sandbox'], dumpio: true });
   }
-  // let launchTime = performance.now();
+  let launchTime = performance.now();
   const page = await playwrightBrowser.newPage();
-  page.on('console', (msg) => {
-    console.log('console',msg);
-  });
-  page.on('pageerror', (msg) => {
-    console.log('pageerror',msg);
-  });
-  page.on('requestfailed', (request) => {
-    console.log('requestfailed',request.url(), request.failure().errorText);
-  }
-  );
-  page.on('response', (response) => {
-    console.log('response',response.url(), response.status());
-  }
-  );
-  page.on('request', (request) => {
-    console.log('request',request.url());
-  }
-  );
-  page.on('load', () => {
-    console.log('Page loaded');
-  }
-  );
-  page.on('domcontentloaded', () => {
-    console.log('DOM content loaded');
-  }
-  );
-  page.on('close', () => {
-    console.log('Page closed');
-  }
-  );
-  page.on('error', (error) => {
-    console.log('error', error);
-  }
-  );
   try {
     await page.setViewportSize({ width: 600, height: 600 });
     const response = await page.goto(url, { waitUntil: 'domcontentloaded', timeout: 10000 });
@@ -156,10 +48,10 @@ async function renderContentPlaywright(url) {
     }
     await page.waitForLoadState('networkidle', { timeout: 10000 });
     const screenshotBuffer = await page.screenshot({ fullPage: true });
-    // console.log('Screenshot captured in memory.');
-    // let endTime = performance.now();
-    // console.log('Launch time:', launchTime - startTime);
-    // console.log('Render time:', endTime - launchTime);
+    console.log('Screenshot captured in memory.');
+    let endTime = performance.now();
+    console.log('Launch time:', launchTime - startTime);
+    console.log('Render time:', endTime - launchTime);
     return screenshotBuffer;
   } catch (error) {
     console.error('Error rendering content:', error);
@@ -170,11 +62,11 @@ async function renderContentPlaywright(url) {
 }
 
 async function renderContentPuppeteer(url) {
-  // let startTime = performance.now();
+  let startTime = performance.now();
   if (!puppeteerBrowser) {
     puppeteerBrowser = await puppeteer.launch({ headless: true, args: ['--no-sandbox'] });
   }
-  // let launchTime = performance.now();
+  let launchTime = performance.now();
   const page = await puppeteerBrowser.newPage();
   try {
     await page.setViewport({ width: 600, height: 600 });
@@ -184,10 +76,10 @@ async function renderContentPuppeteer(url) {
     }
     await page.waitForNetworkIdle({ timeout: 10000 });
     const screenshotBuffer = await page.screenshot({ fullPage: true });
-    // console.log('Screenshot captured in memory.');
-    // let endTime = performance.now();
-    // console.log('Launch time:', launchTime - startTime);
-    // console.log('Render time:', endTime - launchTime);
+    console.log('Screenshot captured in memory.');
+    let endTime = performance.now();
+    console.log('Launch time:', launchTime - startTime);
+    console.log('Render time:', endTime - launchTime);
     return screenshotBuffer;
   } catch (error) {
     console.error('Error rendering content:', error);
@@ -227,3 +119,40 @@ process.on("SIGINT", async () => {
 process.on("SIGTERM", async () => {
   await shutdown();
 });
+
+
+
+// page.on('console', (msg) => {
+//   console.log('console',msg);
+// });
+// page.on('pageerror', (msg) => {
+//   console.log('pageerror',msg);
+// });
+// page.on('requestfailed', (request) => {
+//   console.log('requestfailed',request.url(), request.failure().errorText);
+// }
+// );
+// page.on('response', (response) => {
+//   console.log('response',response.url(), response.status());
+// }
+// );
+// page.on('request', (request) => {
+//   console.log('request',request.url());
+// }
+// );
+// page.on('load', () => {
+//   console.log('Page loaded');
+// }
+// );
+// page.on('domcontentloaded', () => {
+//   console.log('DOM content loaded');
+// }
+// );
+// page.on('close', () => {
+//   console.log('Page closed');
+// }
+// );
+// page.on('error', (error) => {
+//   console.log('error', error);
+// }
+// );
