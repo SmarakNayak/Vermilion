60afce1ee031b637dc00b0b759b1b60334d4bc08
Smarak Nayak <miltu.s.nayak@gmail.com>
2025-03-09 23:23:09 +1100

    only render if html, can add ?force_render later if required

--- app/server/server.js
+++ app/server/server.js
@@ -85,24 +85,14 @@ const server = Bun.serve({
   routes: {
     '/': new Response('If Bitcoin is to change the culture of money, it needs to be cool. Ordinals was the missing piece. The path to $1m is preordained'),
     '/rendered_content/:id': async req => {
-      let ss = await renderContentPuppeteer(apiBaseUrl + "/content/" + req.params.id);
-      if (!ss) {
-        return new Response('Error rendering content', { status: 404 });
-      }
-      return new Response(ss, {
-        headers: { 'Content-Type': 'image/png' },
-      });
+      let metadata =  await fetch(apiBaseUrl + "/api/inscription_metadata/" + req.params.id);
+      let metadataJson = await metadata.json();
+      return getRenderedContentResponse(metadataJson.id, metadataJson.content_type);  
     },
     '/rendered_content_number/:number': async req => {
       let metadata =  await fetch(apiBaseUrl + "/api/inscription_metadata_number/" + req.params.number);
       let metadataJson = await metadata.json();
-      let ss = await renderContentPuppeteer(apiBaseUrl + "/content/" + metadataJson.id);
-      if (!ss) {
-        return new Response('Error rendering content', { status: 404 });
-      }
-      return new Response(ss, {
-        headers: { 'Content-Type': 'image/png' },
-      });
+      return getRenderedContentResponse(metadataJson.id, metadataJson.content_type);     
     },
     '/block_icon/:block': async req => {
       const [row] = await sql`SELECT id, content_type FROM ordinals 
@@ -111,20 +101,7 @@ const server = Bun.serve({
          ORDER BY content_length DESC NULLS LAST
          LIMIT 1`;
       if (!row) return new Response('No inscriptions found in block', { status: 404 });
-
-      if (row.content_type.startsWith('text/html')) {
-        let ss = await renderContentPuppeteer(apiBaseUrl + "/content/" + row.id);
-        if (!ss) return new Response('Error rendering content', { status: 404 });
-        return new Response(ss, {
-          headers: { 'Content-Type': 'image/png' },
-        });
-      } else {
-        let image = await fetch(apiBaseUrl + "/content/" + row.id, {
-          decompress: false
-        });
-        if (!image.ok) return new Response('Image fetch failed', { status: image.status });
-        return image;
-      }
+      return getRenderedContentResponse(row.id, row.content_type);     
     },
     '/sat_block_icon/:block': async req => {
       const [row] = await sql`SELECT id, content_type FROM ordinals 
@@ -133,20 +110,7 @@ const server = Bun.serve({
          ORDER BY content_length DESC NULLS LAST
          LIMIT 1`;
       if (!row) return new Response('No inscriptions found in block', { status: 404 });
-      
-      if (row.content_type.startsWith('text/html')) {
-        let ss = await renderContentPuppeteer(apiBaseUrl + "/content/" + row.id);
-        if (!ss) return new Response('Error rendering content', { status: 404 });
-        return new Response(ss, {
-          headers: { 'Content-Type': 'image/png' },
-        });
-      } else {
-        let image = await fetch(apiBaseUrl + "/content/" + row.id, {
-          decompress: false
-        });
-        if (!image.ok) return new Response('Image fetch failed', { status: image.status });
-        return image;
-      }
+      return getRenderedContentResponse(row.id, row.content_type);      
     },
   }
 });
@@ -179,6 +143,22 @@ async function renderContentPuppeteer(url) {
     await page.close();
     // Release the browser back to the pool
     browserPool.releaseBrowser(browser);
+  }
+}
+
+async function getRenderedContentResponse(id, content_type) {
+  if (content_type.startsWith('text/html')) {
+    let ss = await renderContentPuppeteer(apiBaseUrl + "/content/" + id);
+    if (!ss) return new Response('Error rendering content', { status: 404 });
+    return new Response(ss, {
+      headers: { 'Content-Type': 'image/png' },
+    });
+  } else {
+    let content = await fetch(apiBaseUrl + "/content/" + id, {
+      decompress: false
+    });
+    if (!content.ok) return new Response('Content fetch failed', { status: content.status });
+    return content;
   }
 }
 
