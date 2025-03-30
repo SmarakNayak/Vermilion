96e337a076b8da7cf75ded1f408972ad7c532615
Smarak Nayak <miltu.s.nayak@gmail.com>
2025-03-30 18:00:08 +1100

    goodbye vite, we hardly knew ye

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { ThemeProvider } from 'styled-components'
 import theme from './styles/theme'
 import Navigation from './navigation/routes'
-import ReactGA from 'react-ga4';
+const ReactGA = require('react-ga4').default; // ESM import syntax doesn't work for this package in bun, using old cjs syntax for now
 ReactGA.initialize('G-R5ZRQ7E8Q3');
 
 function App() {
