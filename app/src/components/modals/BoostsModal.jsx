a59bcd56b2a44728e632e68d091a4e909eb0b042
currytay <currytay@gmail.com>
2025-04-28 20:21:24 +1000

    add usecallback import to boosts modal

--- app/src/components/modals/BoostsModal.jsx
+++ app/src/components/modals/BoostsModal.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useCallback } from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import theme from '../../styles/theme';
