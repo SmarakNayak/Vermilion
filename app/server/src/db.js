c6d456f53ed5cf820f9cb1741e135cafc3bbf127
Smarak Nayak <miltu.s.nayak@gmail.com>
2025-05-04 23:35:02 +1000

    create db schema for recording boosts and sweeps

--- app/server/src/db.js
+++ app/server/src/db.js
@@ -24,6 +24,60 @@ const db = {
           render_status TEXT
         );
       `;
+      await this.sql`
+        CREATE SCHEMA IF NOT EXISTS social;
+      `;
+      await this.sql`
+        CREATE TABLE IF NOT EXISTS social.boosts (
+          boost_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+          -- boost info
+          delegate_id varchar(80) NOT NULL,
+          boost_quantity INT NOT NULL,
+          boost_comment TEXT,
+          platform_address VARCHAR(64),
+          platform_fee bigint,
+          owner_address VARCHAR(64),
+          owner_fee bigint,
+          -- wallet info
+          network VARCHAR(10) NOT NULL,
+          wallet_type VARCHAR(10) NOT NULL,
+          inscription_method VARCHAR(40) NOT NULL,
+          ordinals_address VARCHAR(64) NOT NULL,
+          ordinals_public_key VARCHAR(66) NOT NULL,
+          payment_address VARCHAR(64) NOT NULL,
+          payment_public_key VARCHAR(66) NOT NULL,
+          ephemeral_public_key VARCHAR(66),
+          -- inscription info
+          fee_rate INT NOT NULL,
+          commit_tx_hex TEXT NOT NULL,
+          commit_tx_id VARCHAR(64) NOT NULL,
+          reveal_tx_hex TEXT NOT NULL,
+          reveal_tx_id VARCHAR(64) NOT NULL,
+          reveal_address_script VARCHAR(64) NOT NULL,
+          reveal_tapmerkleroot VARCHAR(64) NOT NULL,
+          reveal_input_value BIGINT NOT NULL,
+          reveal_script TEXT NOT NULL,
+          -- status
+          broadcast_status VARCHAR(10),
+          broadcast_error TEXT,
+          commit_tx_status VARCHAR(10),
+          reveal_tx_status VARCHAR(10),
+
+          timestamp TIMESTAMP DEFAULT NOW(),
+        );
+      `;
+      await this.sql`
+        CREATE TABLE IF NOT EXISTS social.ephemeral_sweeps (
+          sweep_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+          boost_id UUID NOT NULL REFERENCES social.boosts(boost_id),
+          fee_rate INT NOT NULL,
+          reveaL_sweep_tx_hex TEXT NOT NULL,
+          broadcast_status VARCHAR(10),
+          broadcast_error TEXT,
+          reveal_sweep_tx_status VARCHAR(10),
+          broadcast_timestamp TIMESTAMP DEFAULT NULL,
+        );
+      `;
     } catch (err) {
       console.error(err);
       throw new Error('Error setting up database: ' + err.message);
@@ -92,6 +146,22 @@ const db = {
   async getRenderedContent(id) {
     const [row] = await this.sql`SELECT content FROM rendered_content WHERE id = ${id}`;
     return row;
+  },
+
+  // Social
+  async recordBoost(boost) {
+    try {
+      await this.sql`INSERT INTO social.boosts ${this.sql(boost)};`;
+    } catch (err) {
+      throw new Error('Error during insert: ', { cause: err });
+    }
+  },
+  async recordEphemeralSweep(sweep) {
+    try {
+      await this.sql`INSERT INTO social.ephemeral_sweeps ${this.sql(sweep)};`;
+    } catch (err) {
+      throw new Error('Error during insert: ', { cause: err });
+    }
   },
 
   // Shutdown
