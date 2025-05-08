1abb4edeeb9601723d2d344af3af7b8fb9d0945f
currytay <currytay@gmail.com>
2025-05-09 00:04:57 +1000

    add onchain metadata to children page

--- app/src/pages/Children.jsx
+++ app/src/pages/Children.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import styled from 'styled-components';
 import { useParams, Link } from "react-router-dom";
 import { theme } from '../styles/theme';
 
@@ -24,7 +25,7 @@ import InscriptionIcon from '../components/InscriptionIcon';
 import Tag from '../components/Tag';
 
 // import icons
-import { GalleryIcon } from '../components/common/Icon';
+import { ChevronDownSmallIcon, GalleryIcon } from '../components/common/Icon';
 
 // import utils
 import { 
@@ -35,11 +36,14 @@ import {
   shortenBytesString,
   shortenDate 
 } from '../utils/format';
+import { ChevronRightSmallIcon } from '../components/common/Icon/icons/ChevronRightSmallIcon';
 
 const Children = ({ setParentNumbers }) => {
   const [baseApi, setBaseApi] = useState(null); 
   let { number } = useParams();
   const [metadata, setMetadata] = useState(null);
+  const [inscriptionMetadata, setInscriptionMetadata] = useState(null);
+  const [metadataVisibility, setMetadataVisibility] = useState(false);
   const [numberVisibility, setNumberVisibility] = useState(true);
   const [filterVisibility, setFilterVisibility] = useState(false);
   const [zoomGrid, setZoomGrid] = useState(true);
@@ -60,6 +64,14 @@ const Children = ({ setParentNumbers }) => {
         // Pass parent numbers up to the parent component
         if (metadataJson?.parent_numbers) {
           setParentNumbers(metadataJson.parent_numbers);
+        }
+
+        // Fetch inscription metadata if there is only one parent
+        if (metadataJson?.parent_numbers.length === 1) {
+          const parentId = metadataJson.parent_numbers[0];
+          const inscriptionMetadataResponse = await fetch(`/api/inscription_metadata/${number}`);
+          const inscriptionMetadataJson = await inscriptionMetadataResponse.json();
+          setInscriptionMetadata(inscriptionMetadataJson);
         }
         
       } catch (error) {
@@ -155,6 +167,35 @@ const Children = ({ setParentNumbers }) => {
             <Tag isLarge={true} value={metadata?.total_inscription_size ? shortenBytesString(metadata.total_inscription_size) : 0} category={'Total Size'} />
             <Tag isLarge={true} value={metadata?.total_inscription_fees ? formatSatsString(metadata.total_inscription_fees) : "0 BTC"} category={'Total Fees'} />
           </RowContainer>
+          {(inscriptionMetadata?.on_chain_metadata && Object.keys(inscriptionMetadata.on_chain_metadata).length > 0) && (
+            <RowContainer style={{gap: '.5rem', flexFlow: 'wrap'}}>
+              <MetadataButton onClick={() => setMetadataVisibility(!metadataVisibility)}>
+                {metadataVisibility ? <ChevronDownSmallIcon size={'1.25rem'} /> : <ChevronRightSmallIcon size={'1.25rem'} />}
+                Onchain Metadata
+              </MetadataButton>
+              {metadataVisibility && (
+                <MetadataContainer>
+                  <BorderedTagSection />
+                  <TextContainer>
+                    {Object.entries(inscriptionMetadata?.on_chain_metadata).map(([key, value], index) => {
+                      // Skip entries that are arrays or objects
+                      if (Array.isArray(value) || (typeof value === 'object' && value !== null)) {
+                        return null;
+                      }
+                      
+                      return (
+                        // <Tag key={key} isLarge={true} value={value} category={key} />
+                        <MetadataText key={key}>
+                          {key}
+                          <MetadataValue> {value}</MetadataValue>
+                        </MetadataText>
+                      );
+                    }).filter(Boolean)}
+                  </TextContainer>
+                </MetadataContainer>
+              )}
+            </RowContainer>
+          )}
         </>
       )}
       <HorizontalDivider></HorizontalDivider>
@@ -181,5 +222,91 @@ const Children = ({ setParentNumbers }) => {
     </PageContainer>
   );
 };
+
+const MetadataContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  padding: 0.75rem;
+  background-color: ${theme.colors.background.primary};
+  border-radius: 0.25rem;
+`;
+
+const MetadataButton = styled.button`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 0.5rem;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  margin: 0;
+  font-family: ${theme.typography.fontFamilies.medium};
+  font-size: 1rem;
+  line-height: 1.5rem;
+  color: ${theme.colors.text.secondary};
+  text-decoration: none;
+  transition: all 200ms ease;
+
+  svg {
+    fill: ${theme.colors.text.secondary};
+    transition: all 200ms ease;
+  }
+
+  &:hover {
+    color: ${theme.colors.text.primary};
+    svg {
+      fill: ${theme.colors.text.primary};
+    }
+  }
+
+  &:active {
+    transform: scale(0.98);
+  }
+`;
+
+const BorderedTagSection = styled.div`
+  display: flex;
+  position: relative;
+  padding-left: calc(2px + 0.75rem);
+  
+  &::before {
+    content: '';
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    width: 2px;
+    background-color: ${theme.colors.border};
+    border-radius: 1px;
+  }
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding: 0;
+  margin: 0;
+`;
+
+const MetadataText = styled.p`
+  font-family: ${theme.typography.fontFamilies.medium};
+  font-size: 1rem;
+  line-height: 1.5rem;
+  color: ${theme.colors.text.secondary};
+  margin: 0;
+  padding: 0;
+`;
+
+const MetadataValue = styled.span`
+  font-family: ${theme.typography.fontFamilies.medium};
+  font-size: 1rem;
+  line-height: 1.5rem;
+  color: ${theme.colors.text.primary};
+  margin: 0;
+  padding: 0;
+`;
 
 export default Children;
