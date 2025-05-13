b6a626a2a979bffd5a2968011406f61939293d89
currytay <currytay@gmail.com>
2025-05-13 11:05:06 +1000

    temporarily change access token expiry

--- app/server/src/authenticator.ts
+++ app/server/src/authenticator.ts
@@ -85,7 +85,7 @@ export class Authenticator {
     const token = jwt.sign(
       { address: ordinalsAddress },
       secret,
-      { expiresIn: '1d' }
+      { expiresIn: '30s' }
     );
     return token;
   }
