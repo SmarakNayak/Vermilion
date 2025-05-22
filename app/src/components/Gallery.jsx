c71e3049222542ddef192689a879eb33f84a43c7
Smarak Nayak <miltu.s.nayak@gmail.com>
2025-05-22 10:53:41 +1000

    minor fix for inconsistent state

--- app/src/components/Gallery.jsx
+++ app/src/components/Gallery.jsx
@@ -24,7 +24,7 @@ const Gallery = (props) => {
             collection_symbol={entry.collection_symbol} 
             content_length={entry.content_length}
             id={entry.id} 
-            is_child={entry.parents.length > 0}
+            is_child={entry.parents?.length > 0}
             is_recursive={entry.is_recursive}
             item_name={entry.off_chain_metadata?.name}
             key={entry.number} 
