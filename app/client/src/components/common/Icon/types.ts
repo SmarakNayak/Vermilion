a6a51ab9e240a73f516737cf8dfb68ce895a59db
Smarak Nayak <miltu.s.nayak@gmail.com>
2025-09-01 23:55:32 +1000

    remove excess types.ts

--- app/client/src/components/common/Icon/types.ts
+++
@@ -1,8 +0,0 @@
-export interface IconProps {
-  /** Size of the icon in rem units */
-  size?: string;
-  /** Color of the icon - can use theme colors */
-  color?: string;
-  /** Optional className for styled-components */
-  className?: string;
-}
